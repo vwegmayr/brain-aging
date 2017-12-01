c24dd7847ea2161d2983e652bfd3f603719ba48c
Viktor Wegmayr <vwegmayr@inf.ethz.ch>
2017-12-01 12:30:06 +0100

    Clean up code

--- ml_project/models/classification.py
+++ ml_project/models/classification.py
@@ -30,8 +30,6 @@ class ExampleTF(BaseTF):
         super(ExampleTF, self).__init__(input_fn_config, config, params)
 
     def model_fn(self, features, labels, mode, params, config):
-        #================================================================
-
         input_tensor = tf.cast(features["X"], tf.float32)
         input_tensor = tf.expand_dims(input_tensor, axis=-1)
 
@@ -60,25 +58,32 @@ class ExampleTF(BaseTF):
             activation=None)
 
         probabs = tf.nn.softmax(logits)
+
         predictions = tf.argmax(probabs)
 
         loss = tf.nn.softmax_cross_entropy_with_logits(
             labels=tf.one_hot(labels, depth=4),
             logits=logits)
 
-        """
-        first_hidden_layer = tf.layers.dense(features["X"], 10, activation=tf.nn.relu, name="layer0")
+        optimizer = tf.train.GradientDescentOptimizer(
+            learning_rate=params["learning_rate"])
 
-        second_hidden_layer = tf.layers.dense(first_hidden_layer, 10, activation=tf.nn.relu)
-
-        output_layer = tf.layers.dense(second_hidden_layer, 1)
-
-        mean_output = tf.reduce_mean(output_layer)
-
-        predictions = tf.reshape(output_layer, [-1])
-        """
-
-        if mode == tf.estimator.ModeKeys.PREDICT:
+        train_op = optimizer.minimize(
+            loss=loss, global_step=tf.train.get_global_step())
+        #================================================================
+        if "hooks" in params:
+            training_hooks = parse_hooks(params["hooks"], locals(), self.save_path)
+        else:
+            training_hooks = []
+        #================================================================
+        if mode == tf.estimator.ModeKeys.TRAIN or mode == tf.estimator.ModeKeys.EVAL:
+          return tf.estimator.EstimatorSpec(
+              mode=mode,
+              loss=loss,
+              train_op=train_op,
+              training_hooks=training_hooks)
+          
+        elif mode == tf.estimator.ModeKeys.PREDICT:
             return tf.estimator.EstimatorSpec(
                 mode=mode,
                 predictions={
@@ -88,34 +93,6 @@ class ExampleTF(BaseTF):
                 "predictions": tf.estimator.export.PredictOutput({"predictions": predictions}),
                 "probabs": tf.estimator.export.PredictOutput({"probabs": probabs})
                 })
-
-        #loss = tf.losses.mean_squared_error(labels, predictions)
-
-        optimizer = tf.train.GradientDescentOptimizer(
-            learning_rate=params["learning_rate"])
-
-        train_op = optimizer.minimize(
-            loss=loss, global_step=tf.train.get_global_step())
-
-        #================================================================
-        #eval_metric_ops = {
-        #    "score": tf.metrics.root_mean_squared_error(
-        #        tf.cast(labels, tf.float64), predictions)
-        #}
-
-        #================================================================
-        if "hooks" in params:
-            training_hooks = parse_hooks(params["hooks"], locals(), self.save_path)
-        else:
-            training_hooks = []
-
-        if mode == tf.estimator.ModeKeys.TRAIN or mode == tf.estimator.ModeKeys.EVAL:
-          return tf.estimator.EstimatorSpec(
-              mode=mode,
-              loss=loss,
-              train_op=train_op,
-              #eval_metric_ops=eval_metric_ops,
-              training_hooks=training_hooks)
 
     def score(self, X, y):
         y_pred = self.predict(X)
