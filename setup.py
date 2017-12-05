2fa0e66cfd0f2744b808088bf8d024f99811f657
vwegmayr <vwegmayr@inf.ethz.ch>
2017-12-05 10:12:12 +0100

    Change project name in setup

--- setup.py
+++ setup.py
@@ -17,6 +17,9 @@ def setup():
        https://conda.io/docs/install/quick.html#linux-miniconda-install
 
     """
+
+    PROJECT_NAME = "entrack"
+
     if sys.version_info.major < 3:
         action = getattr(subprocess, "call")
     elif sys.version_info.minor < 5:
@@ -24,18 +27,19 @@ def setup():
     else:
         action = getattr(subprocess, "run")
 
-    action(["conda", "env", "create", "-n", "ml_project", "-f",
+    action(["conda", "env", "create", "-n", PROJECT_NAME, "-f",
             ".environment"])
 
-    action(["bash", "-c", "source activate ml_project && "
+    action(["bash", "-c", "source activate {} && ".format(PROJECT_NAME) +
             "smt init -d {datapath} -i {datapath} -e python -m run.py "
-            "-c error -l cmdline ml_project".format(
-                datapath=normpath('./data'))])
+            "-c error -l cmdline {project_name}".format(
+            datapath=normpath('./data'), project_name=PROJECT_NAME)])
 
     action(["cp", ".example_config.yaml", ".config.yaml"])
 
     print("\n========================================================")
-    print("Type 'source activate ml_project' to activate environment.")
+    print("Type 'source activate {}' ".format(PROJECT_NAME) +
+          "to activate environment.")
     print("==========================================================")
 
 
