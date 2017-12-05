f358a2f59e5d27b7a875a6e8741b5bd46525bd4f
Viktor Wegmayr <vwegmayr@inf.ethz.ch>
2017-12-05 16:57:54 +0100

    Add SimpleTracker
    Former-commit-id: 95f6a285ba602703e040ba3b80beaa5e2191bc77

--- modules/models/trackers.py
+++ modules/models/trackers.py
@@ -2,17 +2,15 @@ import numpy as np
 import tensorflow as tf
 import sklearn as skl
 
-from sklearn.utils.validation import check_array, check_is_fitted
-
 from modules.models.utils import parse_hooks
-from modules.models.base import BaseTF
+from modules.models.base import BaseTracker
 
 from tensorflow.python.saved_model.signature_constants import (
     DEFAULT_SERVING_SIGNATURE_DEF_KEY)
 from tensorflow.python.estimator.export.export_output import PredictOutput
 
 
-class BasicTracker(BaseTF):
+class SimpleTracker(BaseTracker):
     """docstring for ExampleTF"""
     def __init__(
         self,
@@ -20,63 +18,50 @@ class BasicTracker(BaseTF):
         config={},
         params={}):  # noqa: E129
 
-        super(ExampleTF, self).__init__(input_fn_config, config, params)
+        super(SimpleTracker, self).__init__(
+            input_fn_config,
+            config,
+            params)
 
     def model_fn(self, features, labels, mode, params, config):
 
+        blocks = tf.layers.flatten(features["blocks"])
 
+        incoming = tf.layers.flatten(features["incoming"])
 
-        input_tensor = tf.cast(features["blocks"], tf.float32)
-        input_tensor = tf.expand_dims(input_tensor, axis=-1)
-
-        conv_layer = tf.layers.conv1d(
-            input_tensor,
-            filters=16,
-            kernel_size=256,
-            strides=64,
-            activation=tf.nn.relu)
-
-        max_pooled = tf.layers.max_pooling1d(
-            conv_layer,
-            pool_size=64,
-            strides=1)
-
-        flat = tf.layers.flatten(max_pooled)
+        concat = tf.concat([blocks, incoming], axis=1)
 
         dense_layer_1 = tf.layers.dense(
-            flat,
-            units=512,
+            concat,
+            units=2048,
             activation=tf.nn.relu)
 
-        dense_layer_1_norm = tf.norm(dense_layer_1)
-
-        logits = tf.layers.dense(
+        dense_layer_2 = tf.layers.dense(
             dense_layer_1,
-            units=4,
-            activation=None)
+            units=2048,
+            activation=tf.nn.relu)
 
-        probabs = tf.nn.softmax(logits)
+        unnormed = tf.layers.dense(
+            dense_layer_2,
+            units=3,
+            activation=tf.nn.relu)
 
-        predictions = tf.argmax(probabs, axis=1)
+        normed = tf.divide(unnormed, tf.norm(unnormed))
+
+        predictions = normed
         # ================================================================
         if mode == tf.estimator.ModeKeys.PREDICT:
             return tf.estimator.EstimatorSpec(
                 mode=mode,
                 predictions={
-                    "predictions": predictions,
-                    "probabs": probabs},
+                    "predictions": predictions},
                 export_outputs={
                     DEFAULT_SERVING_SIGNATURE_DEF_KEY:
-                    PredictOutput({"predictions": predictions}),
-                    "probabs":
-                    PredictOutput({"probabs": probabs})})
+                    PredictOutput({"predictions": predictions})})
         # ================================================================
-        labels = tf.cast(labels, tf.int32)
-        labels = tf.one_hot(labels, depth=4)
 
-        loss = tf.nn.softmax_cross_entropy_with_logits(
-            labels=labels,
-            logits=logits)
+        loss = -tf.multiply(normed, labels)
+        loss = tf.reduce_sum(loss, axis=1)
         loss = tf.reduce_mean(loss)
 
         optimizer = tf.train.GradientDescentOptimizer(
@@ -100,7 +85,3 @@ class BasicTracker(BaseTF):
                 loss=loss,
                 train_op=train_op,
                 training_hooks=training_hooks)
-
-    def score(self, X, y):
-        y_pred = self.predict(X)
-        return skl.metrics.f1_score(y, y_pred, average="micro")
