b1884b8f8a3f936bf9792f17cc66aa3e71e32b4a
giacomog <giacomog@student.ethz.ch>
2017-12-05 14:09:13 +0100

    Fix test
    Former-commit-id: abf1b4146e59ac3685dcee428d6fa9f94030fec1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,12 +7,12 @@ class TestUtil(unittest.TestCase):
     def test_convert_nii_and_trk_to_npy(self):
         trk_file = "data/iFOD2.trk"
         nii_file = "data/FODl4.nii.gz"
-        path = "test"
+        path = "tests"
         utils.convert_nii_and_trk_to_npy(nii_file, trk_file, block_size=3, path=path, n_samples=100)
-        self.assertTrue(os.path.exists("test/X.npy"))
-        self.assertTrue(os.path.exists("test/y.npy"))
-        os.remove("test/X.npy")
-        os.remove("test/y.npy")
+        self.assertTrue(os.path.exists("tests/X.npy"))
+        self.assertTrue(os.path.exists("tests/y.npy"))
+        os.remove("tests/X.npy")
+        os.remove("tests/y.npy")
 
 
 if __name__ == '__main__':
