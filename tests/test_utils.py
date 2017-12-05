69b464db8e6685462dac1deb35c35ef6df82ebdd
giacomog <giacomog@student.ethz.ch>
2017-12-05 13:39:03 +0100

    Change test file folder

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,8 +5,8 @@ import modules.models.utils as utils
 class TestUtil(unittest.TestCase):
 
     def test_convert_nii_and_trk_to_npy(self):
-        trk_file = "test/iFOD2.trk"
-        nii_file = "test/FODl4.nii.gz"
+        trk_file = "data/iFOD2.trk"
+        nii_file = "data/FODl4.nii.gz"
         path = "test"
         utils.convert_nii_and_trk_to_npy(nii_file, trk_file, block_size=3, path=path, n_samples=100)
         self.assertTrue(os.path.exists("test/X.npy"))
