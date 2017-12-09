4b47896606590d8f823efa5bc04295ae9b580994
Viktor Wegmayr <vwegmayr@inf.ethz.ch>
2017-12-09 10:19:56 +0100

    Add random train/test data generator and adapt tests
    Former-commit-id: d25f1ea44256c8b5795b2f873fea36e7c2063b70

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,48 +3,39 @@ import unittest
 
 import numpy as np
 from sklearn.externals import joblib
-
+from shutil import rmtree
 import modules.models.utils as utils
 
 
-class Test_make_train_set(unittest.TestCase):
+class Test_make_data_sets(unittest.TestCase):
 
     @classmethod
-    def setUpClass(cls):
-        TRK = "data/iFOD2_skip100.trk"
-        NII = "data/FODl4.nii.gz"
-        PATH = "tests"
-
-        utils.make_train_set(
-            NII,
-            TRK,
-            block_size=3,
-            pkl_path=PATH,
-            samples_percent=0.3)
-
-        if os.path.exists("tests_X.pkl"):
-            cls.X = joblib.load("tests_X.pkl")
-        else:
-            cls.X = None
-        if os.path.exists("tests_y.pkl"):
-            cls.y = joblib.load("tests_y.pkl")
-        else:
-            cls.y = None
+    def setUpClass(self):
+        if not os.path.exists("tests/data"):
+            os.mkdir("tests/data")
+        self.paths = utils.make_rand_sets("tests/data")
 
     @classmethod
-    def tearDownClass(cls):
-        if os.path.exists("tests_X.pkl"):
-            os.remove("tests_X.pkl")
-        if os.path.exists("tests_y.pkl"):
-            os.remove("tests_y.pkl")
+    def tearDownClass(self):
+        rmtree("tests/data")
 
     def test_if_pkls_are_created(self):
-        self.assertIsNotNone(self.X)
-        self.assertIsNotNone(self.y)
+        for mode in ["train", "test"]:
+            for _, file in self.paths[mode].items():
+                self.assertTrue(os.path.exists(file))
 
-    def test_X_blocks_shape(self):
-        self.assertEqual(self.X["blocks"].shape, (38794, 3, 3, 3, 15))
-        self.assertEqual(self.y.shape, (38794, 3))
+    def test_train_shapes(self):
+        X = joblib.load(self.paths["train"]["X"])
+        self.assertEqual(X["blocks"].shape, (100, 3, 3, 3, 15))
+        self.assertEqual(X["incoming"].shape, (100, 9))
+
+        y = joblib.load(self.paths["train"]["y"])
+        self.assertEqual(y.shape, (100, 3))
+
+    def test_test_shapes(self):
+        X = joblib.load(self.paths["test"]["X"])
+        self.assertEqual(X["dwi"].shape, (10, 10, 10, 15))
+        self.assertEqual(X["mask"].shape, (10, 10, 10))
 
 
 class Test_aff_to_rot(unittest.TestCase):
