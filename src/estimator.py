804199bd37129a67fd039edcb9311b2831b1cb46
Dany <ETHZ>
2018-01-25 17:30:53 +0100

    Evaluate model every epoch

--- src/estimator.py
+++ src/estimator.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
-from modules.models.base import BaseTF as TensorflowBaseEstimator
-from modules.models.utils import parse_hooks
+import numpy as np
+import pickle as pkl
 
+from modules.models.base import BaseTF as TensorflowBaseEstimator
+from modules.models.utils import parse_hooks, custom_print
 from preprocessing import preprocess_all_if_needed
 from input import train_input, test_input
 from model import Model
@@ -10,9 +12,14 @@ from model import Model
 class Estimator(TensorflowBaseEstimator):
     """docstring for Estimator"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, run_config, *args, **kwargs):
         import features
-        super(Estimator, self).__init__(*args, **kwargs)
+        self.run_config = run_config
+        super(Estimator, self).__init__(
+            config=run_config['tf_run_config'],
+            *args,
+            **kwargs
+        )
         features.all_features.feature_info[features.MRI]['shape'] = \
             self.input_fn_config['data_generation']['image_shape']
         self.feature_spec = {
@@ -22,6 +29,33 @@ class Estimator(TensorflowBaseEstimator):
                 )
             for name, ft_info in features.all_features.feature_info.items()
         }
+
+    def fit_main_training_loop(self, X, y):
+        """
+        Trains and runs validation regularly at the same time
+        """
+        train_fn = self.gen_input_fn(X, y, True, self.input_fn_config)
+        evaluate_fn = self.gen_input_fn(X, y, False, self.input_fn_config)
+        num_epochs = self.run_config['num_epochs']
+        validations_per_epoch = self.run_config['validations_per_epoch']
+        assert(evaluate_fn is not None)
+        # TODO: Support more validations per epoch
+        assert(validations_per_epoch <= 1)
+
+        validation_counter = 0
+        self.evaluations = []
+        for i in range(num_epochs):
+            self.estimator.train(input_fn=train_fn)
+
+            # Check if we need to run validation
+            validation_counter += validations_per_epoch
+            if validation_counter >= 1:
+                validation_counter -= 1
+                self.evaluations.append(
+                    self.estimator.evaluate(input_fn=evaluate_fn)
+                )
+                self.export_evaluation_stats()
+
 
     def score(self, X, y):
         """
@@ -189,5 +223,50 @@ class Estimator(TensorflowBaseEstimator):
                 )
         return _input_fn
 
-    def validate_params(self, params):
-        assert("regression" in params ^ "classification" in params)
+    def export_evaluation_stats(self):
+        """
+        @values is a list of return values of tf.Estimator.evaluate
+        """
+        import matplotlib
+        matplotlib.use('Agg')
+        import matplotlib.pyplot as plt
+
+        if self.evaluations == []:
+            return
+        output_dir = self.config["model_dir"]
+        validations_per_epoch = self.run_config['validations_per_epoch']
+        custom_print('[INFO] Exporting evaluations to "%s"' % output_dir)
+
+        # List of dicts to dict of lists
+        v = dict(zip(
+            self.evaluations[0],
+            zip(*[d.values() for d in self.evaluations])
+        ))
+
+        for label, values in v.items():
+            # Need to skip first value, because loss is not evaluated
+            # at the beginning
+            x_values = np.linspace(
+                0,
+                len(values) / validations_per_epoch,
+                len(values)+1,
+            )
+            plt.plot(
+                x_values[1:],
+                values,
+            )
+            plt.title(label)
+            plt.xlabel('Training iteration')
+            plt.ylabel('%s on validation set' % label)
+            plt.savefig(
+                '%s/eval_%s.png' % (output_dir, label),
+                bbox_inches='tight',
+            )
+            plt.close()
+
+        with open('%s/eval_values.pkl' % (output_dir), 'wb') as f:
+            pkl.dump({
+                'version': 1,
+                'validations_per_epoch': validations_per_epoch,
+                'evaluate': self.evaluations,
+            }, f, pkl.HIGHEST_PROTOCOL)
