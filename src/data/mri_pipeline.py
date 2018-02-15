4275f38735656612b499e7aa62a9936a800dfdf2
Dany <ETHZ>
2018-02-15 11:52:54 +0100

    MRI Pipeline/PPMI: filter out T2 images/too small images

--- src/data/mri_pipeline.py
+++ src/data/mri_pipeline.py
@@ -2,7 +2,26 @@ import re
 import os
 import glob
 import subprocess
+import xml.etree.ElementTree as ET
 from modules.models.utils import custom_print
+
+
+def xml_elem_unique(root, path):
+    e = root.findall(path)
+    assert(len(e) == 1)
+    return e[0].text
+
+
+def filters_match(value, filters):
+    match_type = {
+        'eq': lambda eq: eq == value,
+        'lt': lambda f: float(value) > f,
+        'st': lambda f: float(value) < f,
+    }
+    return all([
+        match_type[type](type_value)
+        for type, type_value in filters.items()
+    ])
 
 
 class MriPreprocessingPipeline(object):
@@ -21,6 +40,7 @@ class MriPreprocessingPipeline(object):
         files_glob,
         extract_image_id_regexp,
         regexp_image_id_group,
+        filter_xml=None,
         shard=None,
         params={},
     ):
@@ -30,9 +50,32 @@ class MriPreprocessingPipeline(object):
         self.files = self.all_files
         self.extract_image_id_regexp = re.compile(extract_image_id_regexp)
         self.regexp_image_id_group = regexp_image_id_group
+        self.image_id_enabled = None
+        if filter_xml is not None:
+            self.filter_xml(**filter_xml)
         if shard is not None:
             self.shard(**shard)
 
+    def filter_xml(self, files, xml_image_id, filters):
+        self.image_id_enabled = set()
+        discarded_count = 0
+        for f in glob.glob(files):
+            tree = ET.parse(f)
+            root = tree.getroot()
+            image_id = int(xml_elem_unique(root, xml_image_id))
+
+            pass_all_filters = True
+            for filter in filters:
+                value = xml_elem_unique(root, filter['key'])
+                if not filters_match(value, filter['value']):
+                    pass_all_filters = False
+                    break
+            if pass_all_filters:
+                self.image_id_enabled.add(image_id)
+            else:
+                discarded_count += 1
+        custom_print('[filter_xml] %s images discarded' % (discarded_count))
+        
     def shard(self, worker_index, num_workers):
         custom_print('SHARDS: Worker %s/%s' % (worker_index+1, num_workers))
         assert(worker_index < num_workers)
@@ -47,9 +90,16 @@ class MriPreprocessingPipeline(object):
         self._mkdir('02_registered')
         custom_print('Applying MRI pipeline to %s files' % (len(self.files)))
         for i, mri_raw in enumerate(self.files):
-            image_id = self.extract_image_id_regexp.match(mri_raw).group(self.regexp_image_id_group)
+            image_id = self.extract_image_id_regexp.match(
+                mri_raw,
+            ).group(self.regexp_image_id_group)
+            image_id = int(image_id)
             custom_print('Image %s/%s [image_id = %s]' % (
                 i, len(self.files), image_id))
+            if self.image_id_enabled is not None:
+                if image_id not in self.image_id_enabled:
+                    custom_print('... Skipped')
+                    continue
 
             mri_brain_extracted = os.path.join(
                 self.path,
