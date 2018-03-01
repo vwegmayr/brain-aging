a627ff0c1d31dcb076a5d79b161e28046fbe006e
Dany <ETHZ>
2018-03-01 10:54:55 +0100

    SessionHookFullTrace: Fixed first_step option when training is restarted

--- src/train_hooks.py
+++ src/train_hooks.py
@@ -30,12 +30,13 @@ class SessionHookFullTrace(tf.train.SessionRunHook):
         self.writer = tf.summary.FileWriter(ckptdir)
         self.trace_level = trace_level
         self.every_step = every_step
-        self._trace = first_step
+        self.first_step = first_step
+        self._trace = False
 
     def begin(self):
         self._global_step_tensor = tf.train.get_global_step()
         if self._global_step_tensor is None:
-            raise RuntimeError("Global step should be created to use _TraceHook.")
+            raise RuntimeError("Global step should be created to use SessionHookFullTrace.")
 
     def before_run(self, run_context):
         if self._trace:
@@ -56,4 +57,6 @@ class SessionHookFullTrace(tf.train.SessionRunHook):
             )
         if (self.every_step is not None and
             not (global_step + 1) % self.every_step):
+            self._trace = True
+        if self.first_step and global_step == 1:
             self._trace = True
