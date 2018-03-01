5df5195b5f674a4ff332b7a600bf449cd78f5146
Dany <ETHZ>
2018-03-01 16:12:55 +0100

    process.sh

---
+++ process.sh
@@ -0,0 +1,15 @@
+TODO_FILES=~/entrack/configs/todo/*.yaml
+DOING_FOLDER="configs/doing"
+DONE_FOLDER="configs/done"
+
+for f in $TODO_FILES
+do
+    name=`basename $f`
+    if [ -f $f ]; then
+        echo "[*] $name ($f)"
+        mv $f $DOING_FOLDER/ && \
+        smt run --config $DOING_FOLDER/$name -S /local/dhaziza/data -a fit && \
+        mv $DOING_FOLDER/$name $DONE_FOLDER/
+        sleep 5
+    fi
+done
