3286c11d95fb65d94e5236a369846220614f732e
mhoerold <mhoerold@student.ethz.ch>
2018-04-30 15:41:34 +0200

    Added bias for logistic regression. Two-level logistic regression.

--- src/linear_estimator.py
+++ src/linear_estimator.py
@@ -95,7 +95,13 @@ class LogisticRegression(BaseTF):
             initializer=tf.contrib.layers.xavier_initializer(seed=43)
         )
 
-        logits = tf.matmul(input_layer, weights)
+        bias = tf.get_variable(
+            name="logistic_bias",
+            shape=[1, self.params["n_classes"]],
+            initializer=tf.contrib.layers.xavier_initializer(seed=43)
+        )
+
+        logits = tf.add(tf.matmul(input_layer, weights), bias, name="logits")
 
         probs = tf.nn.softmax(logits)
         preds = tf.argmax(input=logits, axis=1)
@@ -171,15 +177,34 @@ class TestRetestLogisticRegression(LogisticRegression):
     Allow the following regularization:
         - l2_logits: l2-norm of the difference between test
           and retest logits
+        - l2_sq_logits: squared of l2_logits
+        - l2_probs: l2-norm of the difference between test
+          and retest probabilities
+        - l2_sq_probs: squared of l2_probs
+        - l1_probs: l1-norm of the difference between test
+          and retest probabilities
     """
-    def prediction_nodes(self, features_tensor, labels, params, weights):
-        # Prediction
+    def prediction_nodes(self, features_tensor, labels, params, weights, bias):
+        """
+        Args:
+            - features_tensor: tensor representing features
+            - labels: tensorf representing labels
+            - params: dictionary containg parameters for the model function
+            - weights: weight tensor used to compute logits
+            - bias: bias tensor
+
+        Returns:
+            - input_layer: tensor representing input layer
+            - logits: output tensor of logits computation
+            - probs: output tensor of predicted probabilities
+            - preds: output tensor of predicted classes
+        """
         input_layer = tf.reshape(
             features_tensor,
             [-1, self.params["input_dim"]]
         )
 
-        logits = tf.matmul(input_layer, weights, name="logits")
+        logits = tf.add(tf.matmul(input_layer, weights), bias, name="logits")
 
         probs = tf.nn.softmax(logits, name="probabilities")
         preds = tf.argmax(input=logits, axis=1, name="predictions")
@@ -199,13 +224,20 @@ class TestRetestLogisticRegression(LogisticRegression):
             initializer=tf.contrib.layers.xavier_initializer(seed=43)
         )
 
+        bias = tf.get_variable(
+            name="logistic_bias",
+            shape=[1, self.params["n_classes"]],
+            initializer=tf.contrib.layers.xavier_initializer(seed=43)
+        )
+
         with tf.name_scope("test"):
             input_test, logits_test, probs_test, preds_test, \
                 acc_test = self.prediction_nodes(
                     features["X_test"],
                     labels,
                     params,
-                    weights
+                    weights,
+                    bias
                 )
 
         with tf.name_scope("retest"):
@@ -214,7 +246,8 @@ class TestRetestLogisticRegression(LogisticRegression):
                     features["X_retest"],
                     labels,
                     params,
-                    weights
+                    weights,
+                    bias
                 )
 
         predictions = {
@@ -322,6 +355,188 @@ class TestRetestLogisticRegression(LogisticRegression):
         )
 
 
+class TestRetestTwoLevelLogisticRegression(LogisticRegression):
+    """
+    TODO: make first transformation configurable
+    Use one hidden layer to learn an intermediate representation
+    via a (linear) transformation. This hidden representation
+    can be interpreted as new features which can be regularized
+    on test-retest pairs.
+    """
+    def prediction_nodes(self, features_tensor, labels, params, w_1, w_2,
+                         b_1, b_2):
+        """
+        Args:
+            - features_tensor: tensor representing features
+            - labels: tensorf representing labels
+            - params: dictionary containg parameters for the model function
+            - w1: weight tensor used for first-level transformation
+            - w2: weight tensor used for logistic regression
+            - b1: first-level bias tensor
+            - b2: logistic regression bias tensor
+
+        Returns:
+            - input_layer: tensor representing input layer
+            - logits: output tensor of logits computation
+            - probs: output tensor of predicted probabilities
+            - preds: output tensor of predicted classes
+            - hidden_features: hidden layer activations
+        """
+        input_layer = tf.reshape(
+            features_tensor,
+            [-1, self.params["input_dim"]]
+        )
+
+        hidden_features = tf.add(
+            tf.matmul(input_layer, w_1),
+            b_1,
+            name="hidden_features"
+        )
+        logits = tf.add(
+            tf.matmul(hidden_features, w_2),
+            b_2,
+            name="logits"
+        )
+
+        probs = tf.nn.softmax(logits, name="probabilities")
+        preds = tf.argmax(input=logits, axis=1, name="predictions")
+        accuracy = tf.reduce_mean(
+            tf.cast(tf.equal(preds, labels), tf.float32),
+            name="accuracy"
+        )
+
+        return input_layer, hidden_features, logits, probs, preds, accuracy
+
+    def model_fn(self, features, labels, mode, params):
+        # Construct first-layer weights
+        hidden_weights = tf.get_variable(
+            name="hidden_weights",
+            shape=[self.params["input_dim"], self.params["hidden_dim"]],
+            initializer=tf.contrib.layers.xavier_initializer(seed=43)
+        )
+
+        hidden_bias = tf.get_variable(
+            name="hidden_bias",
+            shape=[1, self.params["hidden_dim"]],
+            initializer=tf.contrib.layers.xavier_initializer(seed=43)
+        )        
+
+        # Construct second-layer weights
+        logistic_weights = tf.get_variable(
+            name="logistic_weights",
+            shape=[self.params["hidden_dim"], self.params["n_classes"]],
+            initializer=tf.contrib.layers.xavier_initializer(seed=43)
+        )
+
+        logistic_bias = tf.get_variable(
+            name="logistic_bias",
+            shape=[1, self.params["n_classes"]],
+            initializer=tf.contrib.layers.xavier_initializer(seed=43)
+        )
+
+        with tf.name_scope("test"):
+            input_test, hidden_features_test, logits_test, probs_test, \
+                preds_test, acc_test = self.prediction_nodes(
+                    features["X_test"],
+                    labels,
+                    params,
+                    hidden_weights,
+                    logistic_weights,
+                    hidden_bias,
+                    logistic_bias
+                )
+
+        with tf.name_scope("retest"):
+            input_retest, hidden_features_retest, logits_retest, probs_retest,\
+                preds_retest, acc_retest = self.prediction_nodes(
+                    features["X_retest"],
+                    labels,
+                    params,
+                    hidden_weights,
+                    logistic_weights,
+                    hidden_bias,
+                    logistic_bias
+                )
+
+        predictions = {
+            "classes_test": preds_test,
+            "classes_retest": preds_retest
+        }
+
+        if mode == tf.estimator.ModeKeys.PREDICT:
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                predictions=predictions
+            )
+
+        # compute cross-entropy loss for test and retest
+        loss_test = tf.losses.sparse_softmax_cross_entropy(
+            labels,
+            logits=logits_test
+        )
+
+        loss_retest = tf.losses.sparse_softmax_cross_entropy(
+            labels=labels,
+            logits=logits_retest
+        )
+
+        # hidden features regularization
+        f1 = hidden_features_test
+        f2 = hidden_features_retest
+
+        param_name = "hidden_features_regularizer"
+        if params[param_name] is None:
+            reg_f = 0
+        elif params[param_name] == "l2":
+            reg_f = regularizer.l2(
+                f1 - f2,
+                name="l2_hidden_features"
+            )
+        elif params[param_name] == "l2_sq":
+            reg_f = regularizer.l2_sq(
+                f1 - f2,
+                name="l2_sq_hidden_features"
+            )
+        else:
+            raise ValueError("Regularizer not found")
+
+        reg_f *= params["lambda_f"]
+
+        # Training
+        loss = loss_test + loss_retest + reg_f
+        optimizer = tf.train.AdamOptimizer(
+            learning_rate=params["learning_rate"]
+        )
+        train_op = optimizer.minimize(loss, tf.train.get_global_step())
+
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                loss=loss,
+                train_op=train_op
+            )
+
+        # Evaluation
+        eval_acc_test = tf.metrics.accuracy(
+            labels=labels,
+            predictions=preds_test
+        )
+        eval_acc_retest = tf.metrics.accuracy(
+            labels=labels,
+            predictions=preds_retest
+        )
+        eval_metric_ops = {
+            'accuracy_test': eval_acc_test,
+            'accuracy_retest': eval_acc_retest
+        }
+
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            eval_metric_ops=eval_metric_ops
+        )
+
+
 class MnistLogisticRegression(LogisticRegression):
     """
     Logistic regression for the MNIST dataset.
@@ -363,6 +578,42 @@ class MnistLogisticRegression(LogisticRegression):
 
 
 class MnistTestRetestLogisticRegression(TestRetestLogisticRegression):
+    """
+    MNIST logistic regression for test-retest data. Loads presampled
+    images from .npy files.
+    """
+    def gen_input_fn(self, X, y=None, train=True, input_fn_config={}):
+        if train:
+            # load training data
+            test, retest, labels = mnist_read.load_test_retest(
+                self.data_params["data_path"],  # path to MNIST root folder
+                self.data_params["train_test_retest"],
+                self.data_params["train_size"],
+                True
+            )
+
+        else:
+            # load test/retest data
+            test, retest, labels = mnist_read.load_test_retest(
+                self.data_params["data_path"],  # path to MNIST root folder
+                self.data_params["test_test_retest"],
+                self.data_params["test_size"],
+                False
+            )
+
+        return tf.estimator.inputs.numpy_input_fn(
+            x={
+                "X_test": test,
+                "X_retest": retest
+            },
+            y=labels,
+            **input_fn_config
+        )
+
+
+class MnistTestRetestTwoLevelLogisticRegression(
+            TestRetestTwoLevelLogisticRegression
+        ):
     """
     MNIST logistic regression for test-retest data. Loads presampled
     images from .npy files.
