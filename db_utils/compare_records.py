514801c8457a9ea8ea565c3eedae53910066d600
mhoerold <mhoerold@student.ethz.ch>
2018-05-15 16:11:24 +0200

    Aggregate test and retest accuracy when plotting

--- db_utils/compare_records.py
+++ db_utils/compare_records.py
@@ -14,6 +14,9 @@ METRICS = config.METRICS
 DATA_PATH = config.DATA_PATH
 PLOT_TAG_LABEL = config.PLOT_TAG_LABEL
 RECORD_LABEL = config.RECORD_LABEL
+AGG_METRICS = config.AGG_METRICS
+X_LABEL = config.X_LABEL
+Y_LABEL = config.Y_LABEL
 
 FILTERS = config.FILTERS
 
@@ -75,47 +78,63 @@ def plot_groups(groups):
     # generate color for each group
     # color = iter(plt.cm.jet(np.linspace(0, 1, len(groups))))
     color = iter(plt.cm.tab10(np.linspace(0, 1, 11)))
-    plt.figure(figsize=(6,4))
+    plt.figure(figsize=(6, 4))
 
     for group in groups:
         for r in group:
             # Load metrics for records
             r.load_metrics(DATA_PATH)
 
-    for m in METRICS:
-        handles = []
-        for group in groups:
-            print("group of size {}".format(len(group)))
-            print(",".join([g.label for g in group]))
-            r = group[0]
-            
+    handles = []
+    for group in groups:
+        print("group of size {}".format(len(group)))
+        print(",".join([g.label for g in group]))
+        r = group[0]
+
+        # Extract group label
+        legend_label = ",".join([r.find_tag(x) for x in PLOT_TAG_LABEL])
+
+        # Check if metrics were measured for this group
+        measured = True
+        for m in AGG_METRICS:
             if m not in r.metrics:
-                continue
-            
-            # Extract group label
-            legend_label = ",".join([r.find_tag(x) for x in PLOT_TAG_LABEL])
-            # x-label for plot
-            x_label = r.metrics[m]["x_label"]
-            # Only ticks for x values
-            x = r.metrics[m]["x"]
-            plt.xticks(x)
+                measured = False
+            else:
+                # Only ticks for x values
+                x = r.metrics[m]["x"]
+                L = len(x)
 
-            # average y values
-            ys = []
-            for r in group:
-                ys.append(r.metrics[m]["y"])
+        if not measured:
+            continue
 
-            ys = np.array(ys)
-            std = np.std(ys, axis=0)
-            mean = np.mean(ys, axis=0)
-            plt.xlabel(x_label)
-            plt.ylabel(m)
-            c = next(color)
-            line = plt.plot(x, mean, c=c, marker='o', linewidth=2,
-                            label=legend_label)
-            plt.plot(x, mean - std, c=c, linestyle="--", linewidth=0.5)
-            plt.plot(x, mean + std, c=c, linestyle="--", linewidth=0.5)
-            handles.append(line)
+        plt.xticks(x)
+
+        # aggregate metrics (average aggregation)
+        for r in group:
+            r.metrics["agg_metric"] = {}
+            r.metrics["agg_metric"]["y"] = np.zeros(L)
+            for m in AGG_METRICS:
+                vals = np.array(r.metrics[m]["y"])
+                r.metrics["agg_metric"]["y"] += vals / len(AGG_METRICS)
+
+        # average y values
+        ys = []
+        for r in group:
+            ys.append(r.metrics["agg_metric"]["y"])
+
+        ys = np.array(ys)
+        std = np.std(ys, axis=0)
+        mean = np.mean(ys, axis=0)
+
+        c = next(color)
+        line = plt.plot(x, mean, c=c, marker='o', linewidth=2,
+                        label=legend_label)
+        plt.plot(x, mean - std, c=c, linestyle="--", linewidth=0.5)
+        plt.plot(x, mean + std, c=c, linestyle="--", linewidth=0.5)
+        handles.append(line)
+
+    plt.xlabel(X_LABEL)
+    plt.ylabel(Y_LABEL)
 
     plt.legend(loc=4, ncol=1)
     plt.grid(True)
