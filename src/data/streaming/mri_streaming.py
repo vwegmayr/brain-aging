fb65f1fa78b1fdd1b9fdd230e7db237afb03922f
Horold Maurice <mhoerold@student.ethz.ch>
2018-06-01 09:46:28 +0200

    Sample pairs of brains same/different patient, same/different diagnosis.

--- src/data/streaming/mri_streaming.py
+++ src/data/streaming/mri_streaming.py
@@ -120,6 +120,7 @@ class MRISamePatientSameAgePairStream(MRISingleStream):
                 age_1 = self.get_age(id_1)
                 age_2 = self.get_age(id_2)
                 if (age_1 == age_2) and (diag_1 == diag_2):
+                    assert id_1 != id_2
                     g = Group([id_1, id_2])
                     g.patient_label = patient_label
                     groups.append(g)
@@ -127,7 +128,130 @@ class MRISamePatientSameAgePairStream(MRISingleStream):
         return groups
 
 
-class MRIDifferentPatientDifferentDiagnosisPairStream(MRISingleStream):
+class MRISamePatientPairStream(MRISingleStream):
+    def group_data(self):
+        n_pairs = self.config["n_pairs"]
+        groups = []
+
+        patient_to_file_ids = self.get_patient_to_file_ids_mapping()
+
+        # Remap patient to diagnose to file_ids
+        patient_to_diags = {}
+        for patient_label in patient_to_file_ids:
+            file_ids = patient_to_file_ids[patient_label]
+            diagnoses = [(fid, self.get_diagnose(fid)) for fid in file_ids]
+
+            dic = {}
+            for t in diagnoses:
+                fid, diag = t
+                if diag not in dic:
+                    dic[diag] = []
+                dic[diag].append(fid)
+
+            patient_to_diags[patient_label] = dic
+
+        # Sample some pairs
+        sampled = set()
+
+        def get_common_diag(p1):
+            s = list(patient_to_diags[p1].keys())
+            possible = []
+            for d in s:
+                if len(patient_to_diags[p1][d]) > 1:
+                    possible.append(d)
+
+            if len(possible) == 0:
+                return None
+
+            idx = self.np_random.randint(0, len(possible))
+            return possible[idx], possible[idx]
+
+        def get_different_diag(p1):
+            s = list(patient_to_diags[p1].keys())
+
+            if len(s) <= 1:
+                return None
+
+            # build possible pairs
+            x1, x2 = self.np_random.randint(0, len(s), 2)
+            while (x1 == x2):
+                x1, x2 = self.np_random.randint(0, len(s), 2)
+
+            return s[x1], s[x2]
+
+        def sample_file_ids(ids):
+            idx = self.np_random.randint(0, len(ids))
+            return ids[idx]
+
+        patient_labels = list(patient_to_diags.keys())
+        if self.shuffle:
+            self.np_random.shuffle(patient_labels)
+
+        n_patients = len(patient_labels)
+        n_train = int(self.config["train_ratio"] * n_patients)
+        n_train_pairs = int(self.config["train_ratio"] * n_pairs)
+        train_labels = patient_labels[:n_train]
+        test_labels = patient_labels[n_train:]
+        print("n_train {}".format(n_train))
+        if self.config["same_diagnosis"]:
+            get_some_diag = get_common_diag
+        else:
+            get_some_diag = get_different_diag
+
+        def sample_pair(i):
+            if i < n_train_pairs:
+                n_patients = len(train_labels)
+                patient_labels = train_labels
+            else:
+                n_patients = len(test_labels)
+                patient_labels = test_labels
+            # sample first patient
+            p1 = self.np_random.randint(0, n_patients)
+            p1 = patient_labels[p1]
+            d = get_some_diag(p1)
+            # sample until both patients have a different diagnose
+            while (d is None):
+                p1 = self.np_random.randint(0, n_patients)
+                p1 = patient_labels[p1]
+                d = get_some_diag(p1)
+
+            d1, d2 = d
+            id1 = sample_file_ids(patient_to_diags[p1][d1])
+            id2 = sample_file_ids(patient_to_diags[p1][d2])
+
+            if self.config["same_diagnosis"]:
+                while (id2 == id1):
+                    id2 = sample_file_ids(patient_to_diags[p1][d2])
+
+            return tuple(sorted((id1, id2)))
+
+        for i in range(n_pairs):
+            pair = sample_pair(i)
+            while pair in sampled:
+                pair = sample_pair(i)
+
+            sampled.add(pair)
+
+        for i, s in enumerate(sampled):
+            lll = s
+            id1 = lll[0]
+            id2 = lll[1]
+            groups.append(Group([id1, id2]))
+            groups[-1].is_train = (i < n_train_pairs)
+            assert self.get_patient_label(id1) == self.get_patient_label(id2)
+            if self.config["same_diagnosis"]:
+                assert self.get_diagnose(id1) == self.get_diagnose(id2)
+            else:
+                assert self.get_diagnose(id1) != self.get_diagnose(id2)
+
+        return groups
+
+    def make_train_test_split(self):
+        # split is done during sampling
+        pass
+
+
+class MRIDifferentPatientPairStream(MRISingleStream):
     def group_data(self):
         n_pairs = self.config["n_pairs"]
         groups = []
@@ -162,7 +286,7 @@ class MRIDifferentPatientDifferentDiagnosisPairStream(MRISingleStream):
                 return None
 
             idx = self.np_random.randint(0, len(inter))
-            return inter[idx]
+            return inter[idx], inter[idx]
 
         def get_different_diag(p1, p2):
             s1 = list(patient_to_diags[p1].keys())
@@ -182,21 +306,39 @@ class MRIDifferentPatientDifferentDiagnosisPairStream(MRISingleStream):
             return ids[idx]
 
         patient_labels = list(patient_to_diags.keys())
-        n_patients = len(patient_labels)
+        if self.shuffle:
+            self.np_random.shuffle(patient_labels)
 
-        def sample_pair():
+        n_patients = len(patient_labels)
+        n_train = int(self.config["train_ratio"] * n_patients)
+        n_train_pairs = int(self.config["train_ratio"] * n_pairs)
+        train_labels = patient_labels[:n_train]
+        test_labels = patient_labels[n_train:]
+        print("n_train {}".format(n_train))
+        if self.config["same_diagnosis"]:
+            get_some_diag = get_common_diag
+        else:
+            get_some_diag = get_different_diag
+
+        def sample_pair(i):
+            if i < n_train_pairs:
+                n_patients = len(train_labels)
+                patient_labels = train_labels
+            else:
+                n_patients = len(test_labels)
+                patient_labels = test_labels
             # sample first patient
             p1 = self.np_random.randint(0, n_patients)
             p1 = patient_labels[p1]
             # sample second patient
             p2 = self.np_random.randint(0, n_patients)
             p2 = patient_labels[p2]
-            d = get_different_diag(p1, p2)
+            d = get_some_diag(p1, p2)
             # sample until both patients have a different diagnose
             while (d is None):
                 p2 = self.np_random.randint(0, n_patients)
                 p2 = patient_labels[p2]
-                d = get_different_diag(p1, p2)
+                d = get_some_diag(p1, p2)
 
             d1, d2 = d
             id1 = sample_file_ids(patient_to_diags[p1][d1])
@@ -204,17 +346,25 @@ class MRIDifferentPatientDifferentDiagnosisPairStream(MRISingleStream):
             return tuple(sorted((id1, id2)))
 
         for i in range(n_pairs):
-            pair = sample_pair()
+            pair = sample_pair(i)
             while pair in sampled:
-                pair = sample_pair()
+                pair = sample_pair(i)
 
             sampled.add(pair)
 
-        for s in sampled:
+        for i, s in enumerate(sampled):
             lll = s
             id1 = lll[0]
             id2 = lll[1]
             groups.append(Group([id1, id2]))
-
+            groups[-1].is_train = (i < n_train_pairs)
+            if self.config["same_diagnosis"]:
+                assert self.get_diagnose(id1) == self.get_diagnose(id2)
+            else:
+                assert self.get_diagnose(id1) != self.get_diagnose(id2)
 
         return groups
+
+    def make_train_test_split(self):
+        # split is done during sampling
+        pass
