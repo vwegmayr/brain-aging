43533d0cf80a760c1db7b6c75e7c1947534a3e3c
mhoerold <mhoerold@student.ethz.ch>
2018-06-03 18:02:19 +0200

    More detailed sampling.

--- src/data/streaming/mri_streaming.py
+++ src/data/streaming/mri_streaming.py
@@ -368,3 +368,127 @@ class MRIDifferentPatientPairStream(MRISingleStream):
     def make_train_test_split(self):
         # split is done during sampling
         pass
+
+
+class MRIDiagnosePairStream(MRISingleStream):
+    def group_data(self):
+        n_pairs = self.config["n_pairs"]
+        groups = []
+
+        patient_to_file_ids = self.get_patient_to_file_ids_mapping()
+
+        # Map diagnosis to patient to file_ids
+        diag_to_patient = {}
+        for patient_label in patient_to_file_ids:
+            file_ids = patient_to_file_ids[patient_label]
+            diagnoses = [(fid, self.get_diagnose(fid)) for fid in file_ids]
+
+            for fid, d in diagnoses:
+                if d not in diag_to_patient:
+                    diag_to_patient[d] = {}
+                if patient_label not in diag_to_patient[d]:
+                    diag_to_patient[d][patient_label] = []
+                diag_to_patient[d][patient_label].append(fid)
+
+        diagnoses_to_sample = self.config["diagnoses"]
+        assert len(diagnoses_to_sample) == 2
+        self.config["same_diagnosis"] = \
+            (diagnoses_to_sample[0] == diagnoses_to_sample[1])
+        # collect patient labels
+        patient_labels = set([])
+        for d in diagnoses_to_sample:
+            labels = set(diag_to_patient[d].keys())
+            patient_labels = patient_labels.union(labels)
+
+        # prepare train-test split
+        patient_labels = list(patient_labels)
+        n_patients = len(patient_labels)
+        n_train = int(self.config["train_ratio"] * n_patients)
+        n_train_pairs = int(self.config["train_ratio"] * n_pairs)
+        train_labels = patient_labels[:n_train]
+        test_labels = patient_labels[n_train:]
+        print("Num of training labels {}".format(len(train_labels)))
+
+        def sample_patient(diagnosis, patient_labels):
+            patients = set(diag_to_patient[diagnosis].keys())
+            sample_from = set(patient_labels).intersection(patients)
+            n = len(sample_from)
+            idx = self.np_random.randint(0, n)
+            return list(sample_from)[idx]
+
+        def sample_file_ids(p1, d1, p2, d2):
+            def _sample(d, p):
+                fids = diag_to_patient[d][p]
+                n = len(fids)
+                idx = self.np_random.randint(0, n)
+                return fids[idx]
+
+            return _sample(d1, p1), _sample(d2, p2)
+
+        def get_pair_sample(patient_labels):
+            d1, d2 = diagnoses_to_sample
+            p1 = sample_patient(d1, patient_labels)
+
+            if self.config["same_patient"] and self.config["same_diagnosis"]:
+                while (len(diag_to_patient[d1][p1]) < 2):
+                    p1 = sample_patient(d1, patient_labels)
+
+                p2 = p1
+            elif self.config["same_patient"]:
+                while (not(p1 in diag_to_patient[d1] and
+                       p1 in diag_to_patient[d2])):
+                    p1 = sample_patient(d1, patient_labels)
+
+                p2 = p1
+            else:
+                # different patient
+                p2 = sample_patient(d2, patient_labels)
+                while (p2 == p1):
+                    p2 = sample_patient(d2, patient_labels)
+
+            # sample file ids
+            fid1, fid2 = sample_file_ids(p1, d1, p2, d2)
+            while (fid1 == fid2):
+                fid1, fid2 = sample_file_ids(p1, d1, p2, d2)
+
+            assert self.get_diagnose(fid1) == d1
+            assert self.get_diagnose(fid2) == d2
+            return fid1, fid2
+
+        def sample_pair(i):
+            if i < n_train_pairs:
+                patient_labels = train_labels
+            else:
+                patient_labels = test_labels
+
+            p = get_pair_sample(patient_labels)
+
+            return tuple(sorted(p))
+
+        sampled = set([])
+        for i in range(n_pairs):
+            pair = sample_pair(i)
+            while pair in sampled:
+                pair = sample_pair(i)
+
+            sampled.add(pair)
+
+        for i, s in enumerate(sampled):
+            lll = s
+            id1 = lll[0]
+            id2 = lll[1]
+            groups.append(Group([id1, id2]))
+            groups[-1].is_train = (i < n_train_pairs)
+            if self.config["same_diagnosis"]:
+                assert self.get_diagnose(id1) == self.get_diagnose(id2)
+            else:
+                assert self.get_diagnose(id1) != self.get_diagnose(id2)
+
+            assert (self.get_patient_id(id1) == self.get_patient_id(id2)) == \
+                self.config["same_patient"]
+
+        return groups
+
+    def make_train_test_split(self):
+        # split is done during sampling
+        pass
--- src/data/streaming/test_streamer.py
+++ src/data/streaming/test_streamer.py
@@ -10,23 +10,8 @@ class TestStreamer(DataTransformer):
         self.sess = tf.Session()
 
     def transform(self, X, y=None):
-        # Stream all images and check if they are None
-        fn = self.streamer.get_input_fn(train=True)
-
-        e = fn()
-        for i in range(200):
-            print(i)
-            r = self.sess.run(e)
-            print(type(r))
-            print(len(r["X_0"]))
-            # for k in r:
-            #     if k != "X_0":
-            #         print(r[k][0])
-            """
-            for k in r:
-                for v in r[k]:
-                    assert v is not 
-            """
+        for group in self.streamer.groups[:10]:
+            print(group)
 
         self.streamer = None
         self.sess = None
