f66c028eb6316db055f3f7ca81513cbcf03d2334
Maurice Hoerold <mhoerold@student.ethz.ch>
2018-06-06 11:39:51 +0200

    Can load data all at once for vector streamer.

--- src/data/streaming/vector_streaming.py
+++ src/data/streaming/vector_streaming.py
@@ -17,3 +17,26 @@ class FeatureVectorStream(MRISingleStream):
             fid = group.file_ids[0]
             p = self.get_file_path(fid)
             group.is_train = (p in train_paths)
+
+    def get_data_matrices(self, train=True):
+        X = []
+        Y = []
+        target_key = self.config["target_label_key"]
+        groups = [group for group in self.groups if group.is_train == train]
+
+        for g in groups:
+            paths = [self.get_file_path(fid) for fid in g.file_ids]
+            vecs = [self.load_sample(p) for p in paths]
+            vecs = np.array(vecs).ravel()
+            X.append(vecs)
+
+            # collect labels
+            labs = [self.get_meta_info_by_key(fid, target_key)
+                    for fid in g.file_ids]
+
+            Y.append(np.array(labs).ravel())
+
+        X = np.array(X)
+        Y = np.array(Y)
+
+        return X, Y
