06b99f143b8cec194d8c051dede5ae747b71d7f4
mhoerold <mhoerold@student.ethz.ch>
2018-07-27 18:44:39 +0200

    Evaluate model computing NCC and plotting difference map vs GT.

--- src/baum_vagan/vagan/model_wrapper.py
+++ src/baum_vagan/vagan/model_wrapper.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from src.baum_vagan.vagan.model_vagan import vagan
+from src.baum_vagan.utils import ncc
 
 
 def import_string(s):
@@ -81,10 +82,64 @@ class VAGanWrapper(object):
             plt.imshow(np.squeeze(morphed[0]), cmap='gray')
             plt.show()
 
+    def plot_evaluation(self, ad_in, morphed, delta_gt, mask):
+        fig = plt.figure()
+        sc = ncc(mask, delta_gt)
+        fig.suptitle("NCC = {}".format(sc), fontweight='bold')
+        ax1 = fig.add_subplot(221)
+        ax1.set_title("Generated map")
+        ax1.imshow(mask, cmap='gray')
+
+        ax2 = fig.add_subplot(222)
+        ax2.set_title("Input AD")
+        ax2.imshow(ad_in, cmap='gray')
+
+        ax3 = fig.add_subplot(223)
+        ax3.set_title("GT map")
+        ax3.imshow(delta_gt, cmap='gray')
+
+        ax4 = fig.add_subplot(224)
+        ax4.set_title("Generated AD")
+        ax4.imshow(morphed, cmap='gray')
+
+        axes = [ax1, ax2, ax3, ax4]
+        for ax in axes:
+            ax.axis('off')
+
+        plt.show()
+
+    def evaluate_difference_map(self):
+        exp_config = self.config
+
+        def sampler_AD(bs):
+            return self.data.testAD.next_batch(bs)[0]
+
+        for i in range(10):
+            x = sampler_AD(1)
+            mask = self.vagan.predict_mask(x)
+            print(mask.shape)
+            mask = mask[0, :, :, 0]
+
+            ad_in = x[0, :, :, 0]
+            if exp_config.conditioned_gan:
+                # TODO adapt for brain MRIs
+                delta_gt = x[0, :, :, -1]
+
+            if exp_config.conditioned_gan:
+                if exp_config.use_tanh:
+                    mask = np.tanh(mask)
+                morphed = ad_in + mask
+            else:
+                morphed = ad_in + mask
+                if exp_config.use_tanh:
+                    morphed = np.tanh(morphed)
+
+            self.plot_evaluation(ad_in, morphed, delta_gt, mask)
+
     def transform(self, X=None, y=None):
         # Load model
         path = self.config.trained_model_dir
+        print(path)
         self.vagan.load_weights(log_dir=path)
 
-        self.show_morphed_images()
-        
+        self.evaluate_difference_map()
